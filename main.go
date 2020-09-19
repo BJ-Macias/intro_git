73095959738d46b3573113440f55d5a31b17c71c
Brian MAcias <brian.macias3872@alumnos.udg.mx>
2020-09-18 20:06:17 -0500

    Capturamos la edad e imprimimos la edad + 1

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import "fmt"
 
 func main() {
 	var nombre string
+	var edad int
+
 	fmt.Println("Hola mundo")
 	fmt.Println("Nombre: ")
-	fmt.Scanf("%s", &nombre)
-
+	fmt.Scan(&nombre)
 	fmt.Println("Hola", nombre)
+	fmt.Println("Edad:")
+	fmt.Scan(&edad)
+	println("Vas a cumplir: ", edad + 1)
+
+
 }
