3fadccc262533c87960db02627cbed8cd2ab27c1
Brian MAcias <brian.macias3872@alumnos.udg.mx>
2020-09-26 15:14:56 -0500

    Revert "comentamos el hola mundo" se necesita que imprima el hola mundo This reverts commit b92459a12b641419b2279a4aaf13ff70de515c28.

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main() {
 	char nombre[100];
 	unsigned int edad;
 
-	// printf("hola mundo\n");
+	printf("hola mundo\n");
 	printf("Nombre: ");
 	fgets(nombre, sizeof(nombre), stdin);
 	printf("hola %s", nombre);
